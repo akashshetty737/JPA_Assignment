fb2eb61bd4c5b9feb3cac71f121a0d54a3717d52
Akash Shetty <47499504+akashshetty737@users.noreply.github.com>
2020-01-14 10:28:44 +0530

    Add files via upload

--- JPAProject_ManyToMany-Bi2/src/com/lti/ui/Main.java
+++ JPAProject_ManyToMany-Bi2/src/com/lti/ui/Main.java
@@ -2,7 +2,7 @@ package com.lti.ui;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -42,25 +42,32 @@ public class Main {
 		Book comic = new Book();
 		comic.setISBN(10);
 		comic.setBookTitle("Mowgli");
-		comic.setBookPrice(25);
+		comic.setBookPrice(250);
 		comic.setAuthors(list2);
 		
 		Book drama = new Book();
 		drama.setISBN(20);
 		drama.setBookTitle("FRIENDS");
-		drama.setBookPrice(50);
+		drama.setBookPrice(500);
 		drama.setAuthors(list2);
 		
 		Book action = new Book();
 		action.setISBN(30);
 		action.setBookTitle("Deadpool");
-		action.setBookPrice(35);
+		action.setBookPrice(1000);
 		action.setAuthors(list3);
 		
 		List<Book> list1 = new ArrayList<Book>();
 		list1.add(comic);
 		list1.add(drama);
+	
+		
+		List<Book> list4 = new ArrayList<Book>();
 		list1.add(action);	
+		
+		author1.setBooks(list1);
+		author2.setBooks(list1);
+		author3.setBooks(list4);
 		
 		
 		entityManager.getTransaction().begin();	
@@ -91,11 +98,83 @@ public class Main {
 		System.out.println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 		}
 	}
+	
+	public static void two(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Author Name: ");
+		String name = sc .nextLine();
+		String jpql = "SELECT b FROM Book b JOIN b.authors a WHERE a.authorName ='" +name+"'";
+		TypedQuery tquery = entityManager.createQuery(jpql, Book.class);
+//		tquery.setParameter("name", name);
+		List<Book> b = tquery.getResultList();
+		System.out.println(b);
+				for(Book b1 : b){
+			System.out.println("Book ISBN: " +b1.getISBN());
+			System.out.println("Book Name: " +b1.getBookTitle());
+			System.out.println("Book Price: " +b1.getBookPrice());
+			
+		List<Author> authors = b1.getAuthors();
+			for(Author a: authors){
+				System.out.println("Author Id: " +a.getAuthorId());
+				System.out.println("Author Name: " +a.getAuthorName());
+			}
+			
+		System.out.println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+		}
+		sc.close();
+	}
+	public static void three(){
+	
+		String jpql = "SELECT b FROM Book b  WHERE b.bookPrice BETWEEN 500 AND 1000";
+		TypedQuery tquery = entityManager.createQuery(jpql, Book.class);
+//		tquery.setParameter("name", name);
+		List<Book> b = tquery.getResultList();
+		System.out.println(b);
+				for(Book b1 : b){
+			System.out.println("Book ISBN: " +b1.getISBN());
+			System.out.println("Book Name: " +b1.getBookTitle());
+			System.out.println("Book Price: " +b1.getBookPrice());
+			
+		List<Author> authors = b1.getAuthors();
+			for(Author a: authors){
+				System.out.println("Author Id: " +a.getAuthorId());
+				System.out.println("Author Name: " +a.getAuthorName());
+			}
+			
+		System.out.println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+		}
+
+	}
+	public static void four(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Book id: ");
+		int isbn = sc .nextInt();
+		String jpql = "SELECT a FROM Author a JOIN a.books b WHERE b.ISBN =" +isbn;
+		TypedQuery tquery = entityManager.createQuery(jpql, Author.class);
+//		tquery.setParameter("name", name);
+		List<Author> a = tquery.getResultList();
+		System.out.println(a);
+				for(Author a1 : a){
+		
+			System.out.println("Author Id: " +a1.getAuthorId());
+			System.out.println("Author Name: " +a1.getAuthorName());
+			
+		List<Book> books = a1.getBooks();
+			for(Book b: books){
+				System.out.println("Book ISBN: " +b.getISBN());
+				System.out.println("Book Name: " +b.getBookTitle());
+				System.out.println("Book Price: " +b.getBookPrice());
+				
+			}
+			
+		System.out.println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+		}
+		sc.close();
+	}
 	public static void main(String[] args) {
 		loadData();
-		one();
 		
-		
+		four();
 		
 	}
 
